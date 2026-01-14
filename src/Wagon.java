bfb2dd76e8ba63a3df1db5ecbaa43df57a23a60a
eloua <elouahabimoha2006@gmail.com>
2026-01-14 23:09:41 +0100

    Een Wagon heeft een nummer en een capaciteit.
    De constructor vult die in.
    
    getCapaciteit() geeft de capaciteit terug.

---
+++ src/Wagon.java
@@ -0,0 +1,14 @@
+public class Wagon {
+
+    private int nummer;
+    private int capaciteit;
+
+    public Wagon(int nummer, int capaciteit) {
+        this.nummer = nummer;
+        this.capaciteit = capaciteit;
+    }
+
+    public int getCapaciteit() {
+        return capaciteit;
+    }
+}
